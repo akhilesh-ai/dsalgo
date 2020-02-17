6ce2ac4a28b25bdb11e6b17ffb3e2487337acd9d
Akhilesh Bamhore <akhilesh.bamhore@softwareag.com>
2020-02-17 12:49:01 +0530

    TASK: capitalize each word in a sentence solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -5,7 +5,14 @@
 //   capitalize("I'm a little tea pot") --> 'I'm A Little Tea Pot'
 //   capitalize('sHoRt AnD sToUt') --> 'Short And Stout'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let strArr = str.split(" ");
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] = strArr[i][0].toUpperCase() + strArr[i].slice(1).toLowerCase();
+  }
+  console.log(strArr.join(" "));
+  return strArr.join(" ");
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
